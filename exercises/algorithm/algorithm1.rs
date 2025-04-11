db9af381ed23a3ff4c3be54a7108be0c3aad995c
LWF2016 <capp@DESKTOP-EPIQV4R>
2025-04-11 10:59:50 +0800

    update

--- exercises/algorithm/algorithm1.rs
+++ exercises/algorithm/algorithm1.rs
@@ -2,19 +2,22 @@
 	single linked list merge
 	This problem requires you to merge two ordered singly linked lists into one ordered singly linked list
 */
-// I AM NOT DONE
+
 
 use std::fmt::{self, Display, Formatter};
 use std::ptr::NonNull;
+#[allow(unused_imports)]
 use std::vec::*;
 
 #[derive(Debug)]
-struct Node<T> {
+struct Node<T> 
+{
     val: T,
     next: Option<NonNull<Node<T>>>,
 }
 
-impl<T> Node<T> {
+impl<T> Node<T> 
+{
     fn new(t: T) -> Node<T> {
         Node {
             val: t,
@@ -23,19 +26,22 @@ impl<T> Node<T> {
     }
 }
 #[derive(Debug)]
-struct LinkedList<T> {
+struct LinkedList<T>
+{
     length: u32,
     start: Option<NonNull<Node<T>>>,
     end: Option<NonNull<Node<T>>>,
 }
 
-impl<T> Default for LinkedList<T> {
+impl<T:Clone + std::cmp::PartialOrd> Default for LinkedList<T> 
+{
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl<T> LinkedList<T> {
+impl<T:Clone + std::cmp::PartialOrd> LinkedList<T> 
+{
     pub fn new() -> Self {
         Self {
             length: 0,
@@ -72,11 +78,69 @@ impl<T> LinkedList<T> {
 	pub fn merge(list_a:LinkedList<T>,list_b:LinkedList<T>) -> Self
 	{
 		//TODO
-		Self {
-            length: 0,
-            start: None,
-            end: None,
+        // let mut list_c = LinkedList::<T>::new();
+        
+        // let mut ptr1:u32=0;
+        // let mut ptr2:u32=0;
+
+        // while ptr1 < list_a.length && ptr2 < list_b.length{
+        //     let a = list_a.get(ptr1 as i32).unwrap();
+        //     let b = list_b.get(ptr2 as i32).unwrap();
+        //     if *a<*b {
+        //         list_c.add((*a).clone());
+        //         ptr1+=1;
+        //     }else{
+        //         list_c.add((*b).clone());
+        //         ptr2+=1;
+        //     }
+        // }
+
+        // while ptr1!=list_a.length{
+        //     let a = list_a.get(ptr1 as i32).unwrap();
+        //     list_c.add((*a).clone());
+        //     ptr1+=1;
+        // }
+
+        // while ptr2!=list_b.length{
+        //     let b = list_b.get(ptr2 as i32).unwrap();
+        //     list_c.add((*b).clone());
+        //     ptr2+=1;
+        // }
+
+		// list_c
+
+        let mut list_c = LinkedList::<T>::new();
+
+        let mut ptr1 = list_a.start;
+        let mut ptr2 = list_b.start;
+
+        while let (Some(a), Some(b)) = (ptr1, ptr2){
+            unsafe{
+                if (*a.as_ptr()).val < (*b.as_ptr()).val{
+                    list_c.add((*a.as_ptr()).val.clone());
+                    ptr1=(*a.as_ptr()).next;
+                }else{
+                    list_c.add((*b.as_ptr()).val.clone());
+                    ptr2=(*b.as_ptr()).next;
+                }
+            }
         }
+
+        while let Some(a) = ptr1{
+            unsafe{
+                list_c.add((*a.as_ptr()).val.clone());
+                ptr1=(*a.as_ptr()).next;
+            }
+        }
+
+        while let Some(b) = ptr2{
+            unsafe{
+                    list_c.add((*b.as_ptr()).val.clone());
+                    ptr2=(*b.as_ptr()).next;
+            }
+        }
+
+        list_c
 	}
 }
 
